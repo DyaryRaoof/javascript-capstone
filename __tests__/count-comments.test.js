78cb06dfdc1e2ec8ec098614ead50800220747b4
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-10-13 23:29:16 +0300

    Add second test to countComments function

--- __tests__/count-comments.test.js
+++ __tests__/count-comments.test.js
@@ -4,3 +4,8 @@ test('should equal to 4', () => {
   const commentsList = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
   expect(countComments(commentsList)).toBe(4);
 });
+
+test('should equal to 0', () => {
+  const commentsList = [];
+  expect(countComments(commentsList)).toBe(0);
+});
