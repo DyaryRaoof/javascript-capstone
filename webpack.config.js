21fb7d4bffb2909674c24ea73c89bc3f3e01c615
leolpaz <leonardolpaz95@gmail.com>
2021-10-14 11:54:27 -0300

    Update README file, and rebuild the dist in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
